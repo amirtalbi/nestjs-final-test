8914d4b5be798cc020827da8340b1631e4f35c1a
Amir Talbi <amirotalbi94@gmail.com>
2024-05-22 10:20:27 +0200

    lint: removing unused code

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,4 +1,4 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Task } from './task.entity';
@@ -15,27 +15,14 @@ export class TaskService {
         userId: number,
         priority: number,
     ): Promise<Task> {
-        if (!name || !userId || !priority) {
-            throw new HttpException(
-                'Invalid task data',
-                HttpStatus.BAD_REQUEST,
-            );
-        }
         return this.tasksRepository.save({ name, userId, priority });
     }
 
     async getTaskByName(name: string): Promise<Task> {
-        const task = await this.tasksRepository.findOne({ where: { name } });
-        if (!task) {
-            throw new HttpException('Task not found', HttpStatus.NOT_FOUND);
-        }
-        return task;
+        return await this.tasksRepository.findOne({ where: { name } });
     }
 
     async getUserTasks(userId: number): Promise<Task[]> {
-        if (!userId) {
-            throw new HttpException('Invalid user ID', HttpStatus.BAD_REQUEST);
-        }
         return this.tasksRepository.find({ where: { userId } });
     }
 
